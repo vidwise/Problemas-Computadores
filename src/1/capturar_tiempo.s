142fd1d7ecf516b767e050c953776386cd601d5b
Aleix Mariné Tena <aleixaretra@gmail.com>
2022-09-29 01:40:46 +0200

    Problem 1: Capturar_tiempo() solved

--- src/1/capturar_tiempo.s
+++ src/1/capturar_tiempo.s
@@ -1,30 +1,38 @@
 
 @; Me entra puntero a char de 6 pos en r0
 capturar_tiempo:
-push{r0-r3, lr}
+push{r0-r4, lr}
     bl iniciar_RTC
     mov r2, r0  @; backup en r2 @ base array salida
     mov r0, #0x26
     bl enviar_RTC
 
     @; Empieza bucle
-    mov r1, #0  @; Indice array
+    mov r1, #-1  @; Indice array
+    mov r4, #0  @; weekday found 
 
     .bucle1:
 
     bl recibir_RTC  @; r0 tiene el valor
 
     cmp r1, #3
-    cmpeq r3, #1
-    beq .FiException
-    moveq r3, #1
-    beq .bucle1
-    .FiException:
+    bne .sumAndNext 
+    
+    @; we are receiving the fourth trash value or the fifth correct value
+    cmp r4, #1
+    beq .next  @; we have good value in r0, and the bad value is in the position r3 of the array. Overwrite the value by no incrementing index
+    
+    @; currently the bad value in r0
+    mov r4, #1
+    add r1, #1  @; this and the following instruction could be only "bne .sumAndNext" with one less instruction but doing a useless memory access by writing the trash values temporarily into the array
+    bne .bucle1
 
-    strb r0, [r2, r1]
+    .sumAndNext
     add r1, #1
+    .next:
+    strb r0, [r2, r1]
 
-    b .bucle
+    b .bucle1
 
     bl parar_RTC
-pop{r0-r3, pc}
+pop{r0-r4, pc}
