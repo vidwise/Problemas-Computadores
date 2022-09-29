999dd16ff7118619603f6177987a9f51e1484507
Aleix Mariné Tena <aleixaretra@gmail.com>
2022-09-29 02:03:12 +0200

    Update capturar_tiempo.s

--- src/1/capturar_tiempo.s
+++ src/1/capturar_tiempo.s
@@ -11,28 +11,28 @@ push{r0-r4, lr}
     mov r1, #-1  @; Indice array
     mov r4, #0  @; weekday found 
 
-    .bucle1:
+    .loop:
 
     bl recibir_RTC  @; r0 tiene el valor
 
-    cmp r1, #3
+    cmp r1, #2  @; we check with 2 because the index is equal to the num of signals -1 (for the offset) -1 (for counting with 0), to check for the fourth signal
     bne .sumAndNext 
     
     @; we are receiving the fourth trash value or the fifth correct value
     cmp r4, #1
-    beq .next  @; we have good value in r0, and the bad value is in the position r3 of the array. Overwrite the value by no incrementing index
+    beq .sumAndNext  @; we have good value in r0, keep iterating
     
     @; currently the bad value in r0
-    mov r4, #1
-    add r1, #1  @; this and the following instruction could be only "bne .sumAndNext" with one less instruction but doing a useless memory access by writing the trash values temporarily into the array
-    bne .bucle1
+    mov r4, #1  @; now we know we have seen the trash value
+    bne .loop  @; continue 
 
     .sumAndNext
     add r1, #1
     .next:
     strb r0, [r2, r1]
 
-    b .bucle1
+    cmp r1, #5
+    bne .loop
 
     bl parar_RTC
 pop{r0-r4, pc}
