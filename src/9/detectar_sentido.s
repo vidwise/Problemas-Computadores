50b4b5d38e6f6bcc9b8ebf3fc57491ee3193f54f
AleixMT <aleixaretra@gmail.com>
2022-10-10 09:01:54 +0200

    ADDED: problema 9, corregido por profe

---
+++ src/9/detectar_sentido.s
@@ -0,0 +1,31 @@
+@; detectar_sentido: verifica el sentido de movimiento dadas dos fases
+@; consecutivas, utilizando los vectores globales s_derecha y s_izquierda
+@; Parámetros:
+@;      char f_ant (R0): fase anterior
+@;      char f_act (R1): fase actual
+@; Resultado:
+@;      R0 : +1 si derecha, -1 si izquierda, 0 si no coincide con ningún
+@;      sentido.
+detectar_sentido:
+        push {r2, r3, lr}
+
+        ldr r2, =s_derecha
+        ldrb r3, [r2, r0]           @; R3 = s_derecha[f_ant]
+        cmp r1, r3
+        bne .Lno_derecha            @; if (f_act = s_derecha[f_ant])
+        mov r0, #1                  @; return(1);
+        b .Lfin_detectar_sentido
+
+    .Lno_derecha:                   @; else
+        ldr r2, =s_izquierda
+        ldrb r3, [r2, r0]           @; R3 = s_izquierda[f_ant]
+        cmp r1, r3
+        bne .Lno_izquierda          @; if (f_act == s_izquierda[f_ant])
+        mov r0,                     #-1 @; return(-1);
+        b .Lfin_detectar_sentido
+
+    .Lno_izquierda:
+        mov r0, #0                  @; else return(0);
+
+    .Lfin_detectar_sentido:
+        pop {r2, r3, pc}
---
+++ src/9/main.c
@@ -0,0 +1,35 @@
+#define posX1 10        // posiciones X de las raquetas (fijas)
+#define posX2 240
+
+short posY1 = 96;       // posiciones Y de las raquetas (variables)
+short posY2 = 96;
+
+char s_derecha[] = {1, 3, 0, 2};        // indicadores de la fase siguiente
+char s_izquierda[] = {2, 0, 3, 1};      // para cada sentido de giro
+
+char f_ant1 = 0;        // fases anteriores (una por raqueta)
+char f_ant2 = 0;
+
+char dibujar1 = 0;      // indicadores de necesidad de redibujar
+char dibujar2 = 0;      // cada una de las raquetas
+
+int main()
+{
+    inicializaciones();
+    do
+    {
+        tareas_independientes();
+        swiWaitForVBlank();
+        if (dibujar1)
+        {
+            dibujar_raqueta(posX1, posY1);
+            dibujar1 = 0;
+        }
+        if (dibujar2)
+        {
+            dibujar_raqueta(posX2, posY2);
+            dibujar2 = 0;
+        }
+    } while (1);
+    return(0);
+}
