fdc7ad66e070df3d871c54e3aa4621bed26b3c05
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2022-12-07 18:49:19 +0100

    Arey: Corrección de los errores detectados en el problema 35.

--- 35/RSI_VBlank.s
+++ 35/RSI_VBlank.s
@@ -6,7 +6,7 @@ RSI_vblank:
 	ldr r2, =fraccion
 	ldr r2, [r2]
 	add r0, r2
-	ldr r2, =360
+	mov r2, #360
 	mov r2, r2, lsl #12
 	cmp r0, r2
 	subge r0, r2
@@ -15,9 +15,10 @@ RSI_vblank:
 	ldr r3, =ang_pos
 	mov r0, r0, lsl #2
 	ldrh r1, [r3, r0]
-	ldrh r2, [r3, r0, #2]
+	add r0, #2
+	ldrh r2, [r3, r0]
 	bl SPR_moverSprite
-	ldr r0, #0X07000000		@; #OAM
+	mov r0, #0x07000000		@; mov r0, #OAM
 	mov r1, #1
 	bl SPR_actualizarSprites
 	bl activar_beat
