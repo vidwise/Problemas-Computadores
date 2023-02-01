97d24b52287c62bb42f06617f60b6d7d5ca45d16
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-01 20:11:40 +0100

    Arey: Añadido de líneas en blanco y de comentarios al main y a las RSIs del problema 17.

--- Soluciones/17/main.c
+++ Soluciones/17/main.c
@@ -2,29 +2,30 @@
 // Author: Arey Ferrero Ramos
 //
 
-unsigned short Perimetro;
-unsigned char Nrayos;
-unsigned short Drayos;		// Drayos++ && Drayos = 0
-unsigned short Vinst;		// Vinst = Drayos * (Perimetro / Nrayos) * 2
-unsigned int Vmed;		// Vmed = Tdist / Ttiempo * 2 / 1000 * 3600
-unsigned int Tdist;		// Tdist = Tdist + Perimetro / Nrayos
-unsigned int Ttiempo;		// Ttiempo++
-unsigned char ind;
-unsigned short buffVinst[180];
+unsigned short Perimetro;	// Perímetro de la rueda. Valor fijo.
+unsigned char Nrayos;		// Número de rayos de la rueda. Valor fijo.
+unsigned short Drayos;		// Número de rayos por segundo. Valor variable -> Drayos++ && Drayos = 0
+unsigned short Vinst;		// Velocidad instantánea (cm/s). Valor variable -> Vinst = Drayos * (Perimetro / Nrayos) * 2
+unsigned int Vmed;		// Velocidad media (dam/h). Valor variable -> Vmed = Tdist / Ttiempo * 2 / 1000 * 3600
+unsigned int Tdist;		// Distancia total (cm). Valor variable -> Tdist = Tdist + Perimetro / Nrayos
+unsigned int Ttiempo;		// Tiempo total (semisegundos). Valor variable -> Ttiempo++
+unsigned char ind;		// Índice posición actual buffer Vinst.
+unsigned short buffVinst[180];			
 
 void main()
 {
-	inicializaciones();
-	do {
+	inicializaciones();			
+	do 
+	{
 		tareas_independientes();
 		scanKeys();
 		if (keysDown() == KEY_START)
-		{
-			Drayos = 0;
+		{					// Si se ha pulsado la tecla start
+			Drayos = 0;				// Reiniciar los contadores.
 			Tdist = 0;
 			Ttiempo = 0;
 			for (ind = 0; ind < 180; ind++)
-			{
+			{					// Reiniciar el buffer con las velocidades instantáneas.
 				buffVinst[ind] = 0;
 			}
 			ind = 0;
