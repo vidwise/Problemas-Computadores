224fcc8022a347a6d4e903e332d58f2f96c90c69
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-04 14:22:30 +0100

    Arey: Añadido de comentarios al main y a las rutinas en ensamblador del problema 12.

--- Soluciones/12/RSI_Ping.s
+++ Soluciones/12/RSI_Ping.s
@@ -7,23 +7,23 @@ RSI_Ping:
     push {r0-r2, lr}
     
     ldr r2, =fase
-    ldrb r0, [r2]
+    ldrb r0, [r2]              @; Se carga la fase. 
     cmp r0, #0
     bne .Lcapturar_tiempo
-    
-    bl cpuStartTiming    
-    mov r0, #1
+                               @; En la fase 0 del programa
+    bl cpuStartTiming          @; Se inicia el contaje de tics.
+    mov r0, #1                 @; Finaliza la fase 0 del programa.
     b .Lfin_capturar_tiempo
 
-.Lcapturar_tiempo:
-    bl cpuGetTiming
+.Lcapturar_tiempo:             @; En la fase 1 del programa
+    bl cpuGetTiming            @; se captura una muestra de tiempo (tics).
     ldr r1, =25135
-    sub r0, r1
+    sub r0, r1                 @; Se resta 25513 tics a la muestra de tiempo para descontar el tiempo que tarda el dispositivo en generar la ráfaga.
     ldr r1, =t_in
-    str r0, [r1]    
-    mov r0, #2
+    str r0, [r1]               @; Se guarda la muestra de tiempo en el vector de muestras de tiempo.    
+    mov r0, #2                 @; Finaliza la fase 1 del programa.
 
 .Lfin_capturar_tiempo:
-    strb r0, [r2]
+    strb r0, [r2]              @; Se guarda la nueva fase del programa.
     
     pop {r0-r2, pc}
--- Soluciones/12/calcular_distancia.s
+++ Soluciones/12/calcular_distancia.s
@@ -8,14 +8,14 @@
 calcular_distancia:
     push {r1, r3, lr}
     
-    mov r1, r0, lsr #1
+    mov r1, r0, lsr #1         @; Se divide el número de tics entre 2.
     mov r0, #5
-    mul r0, r1, r0
+    mul r0, r1, r0             @; Se multiplica el número de tics por 5
     
     ldr r1, =168000000
-    swi 9
+    swi 9                      @; y se divide entre 168000000 para pasarlo a segundos.
     
     ldr r1, =34000
-    mul r0, r1, r0
+    mul r0, r1, r0             @; Se multiplica el tiempo por 34000 para obtener la distancia (cm).
     
     pop {r1, r3, pc}
