65c870ce83a9950a0c8b627733ac54ad50287093
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-04 12:13:36 +0100

    Arey: Sustitución de los tabuladores por espacios para cumplir con las guías oficiales de estilo de programación.

--- Soluciones/25/RSI_timer1.s
+++ Soluciones/25/RSI_timer1.s
@@ -2,28 +2,29 @@
 @; Author: Arey Ferrero Ramos
 @;
 
+    @; RSI del timer 1.
 RSI_Timer1:
-	push {r0-r1, lr}
-	ldr r0, =num_disp		@; Cargamos dir.mem num disp
-	ldrb r1, [r0]
-	cmp r1, #0
-	beq .Ldetener_disparos
-	sub r1, #1
-	strb r1, [r0]
-	ldr r0, =REG_SHOOTER
-	mov r1, #0x0010
-	strh r1, [r0]
-	ldr r0, =status
-	mov r1, #1
-	strb r1, [r0]
-	ldr r0, =actualizar_info
-	strb r1, [r0]
-	ldr r0, =350
-	bl activar_timer2
-	b .Lfin_detener_disparos
+    push {r0-r1, lr}
+    ldr r0, =num_disp            @; Cargamos dir.mem num disp
+    ldrb r1, [r0]
+    cmp r1, #0
+    beq .Ldetener_disparos
+    sub r1, #1
+    strb r1, [r0]
+    ldr r0, =REG_SHOOTER
+    mov r1, #0x0010
+    strh r1, [r0]
+    ldr r0, =status
+    mov r1, #1
+    strb r1, [r0]
+    ldr r0, =actualizar_info
+    strb r1, [r0]
+    ldr r0, =350
+    bl activar_timer2
+    b .Lfin_detener_disparos
 .Ldetener_disparos:
-	mov r0, #0x3
-	bl desactivar_timers
+    mov r0, #0x3
+    bl desactivar_timers
 .Lfin_detener_disparos:
-	pop {r0-r1, pc}
+    pop {r0-r1, pc}
 	
--- Soluciones/25/main.c
+++ Soluciones/25/main.c
@@ -5,58 +5,56 @@
 unsigned char num_disp = 0;         // Número de disparos pendientes
 unsigned char ind_p1 = 0;           // Índice del periodo p1
 unsigned char ind_p2 = 0;           // Índice del periodo p2
-unsigned char status = 0;           // Estado actual (0 ? parado,
-                                    // 1 --> half press,
-						            // 2 --> half+full press)
+unsigned char status = 0;           // Estado actual (0 --> parado,
+                                                   // 1 --> half press,
+                                                   // 2 --> half+full press)
 unsigned char actualizar_info = 0;  // Indica si hay que actualizar información en pantalla
 
-
-void main(){
-	
-	int keys = 0;
-	inicializaciones();
-	do
+void main()
+{
+    int keys = 0;
+    inicializaciones();
+    do
     {
-		tareas_independientes();
-		scanKeys();
-		keys = keysDown();
-		if(keys & KEY_START)
+        tareas_independientes();
+        scanKeys();
+        keys = keysDown();
+        if(keys & KEY_START)
         {
-			if(num_disp == 0)
+            if(num_disp == 0)
             {
-				activar_timers01(1 << ind_p1);
-			}
-			num_disp++;
-			actualizar_info = 1;
-		}
-		if ((keys & KEY_LEFT) && (ind_p1 > 0))
+                activar_timers01(1 << ind_p1);
+            }
+            num_disp++;
+            actualizar_info = 1;
+        }
+        if ((keys & KEY_LEFT) && (ind_p1 > 0))
         {
-			activar_timers01(1 << --ind_p1);  // Primero decrementamos luego desplazamos bits
-			actualizar_info = 1;
-		}
-		if ((keys & KEY_RIGHT) && (ind_p1 < 9))
+            activar_timers01(1 << --ind_p1);        // Primero decrementamos luego desplazamos bits
+            actualizar_info = 1;
+        }
+        if ((keys & KEY_RIGHT) && (ind_p1 < 9))
         {
-			activar_timers01(1 << ++ind_p1);  // Primero incrementamos luego desplazamos bits
-			actualizar_info = 1;
-		}
-		if ((keys & KEY_DOWN) && (ind_p2 > 0))
+            activar_timers01(1 << ++ind_p1);        // Primero incrementamos luego desplazamos bits
+            actualizar_info = 1;
+        }
+        if ((keys & KEY_DOWN) && (ind_p2 > 0))
         {
-			ind_p2--;
-			actualizar_info = 1;
-		}
-		if ((keys & KEY_UP) && (ind_p2 < 9))
+            ind_p2--;
+            actualizar_info = 1;
+        }
+        if ((keys & KEY_UP) && (ind_p2 < 9))
         {
-			ind_p2++;
-			actualizar_info = 1;
-		}
-		swiWaitForVBlank();     // Lo ponemos fuera porque el if casi no ralentiza el programa, no tendremos problemas
-                                // de colisión con el printf.
-							    // Si no hay pulsación de teclas, se disminuirá el consumo de la CPU.
-		if (actualizar_info == 1)
+            ind_p2++;
+            actualizar_info = 1;
+        }
+        swiWaitForVBlank();                         // Se pone fuera porque la evaluación de la condición apenas ralentiza el programa y no existirán problemas de colisiones
+                                                    // Así, se disminuye el consumo de la CPU aunque no haya pulsación de teclas.
+        if (actualizar_info == 1)
         {
-			printf("Periodo 1: %d s\n Periodo 2: %d ms\n Disparos pendientes: %d", 1 << ind_p1, 1 << ind_p2, num_disp);
-			actualizar_info = 0;
-			keys = 0;
-		}
-	} while(1);
+            printf("Periodo 1: %d s\n Periodo 2: %d ms\n Disparos pendientes: %d", 1 << ind_p1, 1 << ind_p2, num_disp);
+            actualizar_info = 0;
+            keys = 0;
+        }
+    } while(1);
 }
