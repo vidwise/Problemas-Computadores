ef72787ccd8a6211a00e756173cb296e429f2814
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-04 19:14:51 +0100

    Arey: Añadido de comentarios al main y las rutinas en ensamblador del problema 8.

--- Soluciones/08/insertar_string.s
+++ Soluciones/08/insertar_string.s
@@ -10,18 +10,18 @@ insertar_string:
     
     ldr r2, =REG_DISPLAY
     bl sincro_display
-    strh r1, [r2]
+    strh r1, [r2]               @; Se fija la dirección base del string con el comando 'DDRAM Address set'
     
     mov r1, #0
 .Lnueva_posicion:
     
-    ldrb r3, [r0, r1]
-    orr r3, #0x200
+    ldrb r3, [r0, r1]           @; Se carga una elemento (byte) del string. 
+    orr r3, #0x200              @; Se construye el comando 'DDRAM or CGRAM write' con un elemento del string.
     bl sincro_display
-    strh r3, [r2]
+    strh r3, [r2]               @; Se escribe el elemento del string en el display.
     
     add r1, #1
-    cmp r1, #MAX_ELEMENTS
+    cmp r1, #MAX_ELEMENTS       @; Se comprueba si se ha llegado al final del string.
     blo .Lnueva_posicion
     
     pop {r0-r3, pc}
--- Soluciones/08/insertar_strings.s
+++ Soluciones/08/insertar_strings.s
@@ -8,13 +8,13 @@
 insertar_strings:
     push {r0-r2, lr}
     
-    mov r2, r1
-    mov r1, #0x080
-    bl insertar_string
+    mov r2, r1                   @; Se traslada la dirección base del primer string para no perderla.           
+    mov r1, #0x080               @; R1 = Comando para fijar la dirección base.        
+    bl insertar_string           @; Se inserta el primer string.
     
     mov r0, r2
     mov r1, #0x0C0
-    bl insertar_string
+    bl insertar_string           @; Se inserta el segundo string.
     
     pop {r0-r2, pc}
 
