854cf18a6a00c86856a2124868fa8c6df1bb1e0c
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-04 16:17:59 +0100

    Arey: Addición de comentarios al main y rutinas en ensamblador del problema 5.

--- Soluciones/05/alternativa_1/RSI_timer0.s
+++ Soluciones/05/alternativa_1/RSI_timer0.s
@@ -8,30 +8,30 @@ RSI_timer0:
     
     ldr r5, =tiempo_restante
     ldrh r4, [r5]
-    subs r4, #1
+    subs r4, #1                   @; Cada vez que ocurre una interrucpción se reduce el tiempo restante para que siga sonando de la nota.
     bne .Lfin_cargar_nota
-    
+                                  @; Si el tiempo restante ha llegado a 0.
     ldr r0, =cambio_nota
     mov r1, #1
-    strb r1, [r0]
+    strb r1, [r0]                 @; Se indica que se ha producido un cambio de nota.
     
     ldr r0, =pos_nota
     ldrh r1, [r0]
-    add r1, #1
+    add r1, #1                    @; Se incrementa el índice del vector de notas.
     ldr r2, =MAX_NOTAS
-    cmp r1, r2
-    movhs r1, #0
+    cmp r1, r2                 
+    movhs r1, #0                  @; y se asegura que no se haya excedido el límite.
     strh r1, [r0]
     
     ldr r0, =musica
     mov r3, #6
-    mul r3, r1, r3
-    ldrh r1, [r0, r3]
-    ldrh r2, [r0, r3, #4]
-    mov r0, #0
+    mul r3, r1, r3                @; Cada nota ocupa 6 Bytes, por lo que el índice del vector de notas debe multiplicarse por 6.
+    ldrh r1, [r0, r3]             @; R1 = Frecuencia.
+    ldrh r2, [r0, r3, #4]         @; R2 = Volumen.     
+    mov r0, #0                    @; R0 = Canal 0.
     bl activar_nota
 
 .Lfin_cargar_nota:
-    strh r4, [r5]
+    strh r4, [r5]                 
     
     pop {r0-r5, pc}
--- Soluciones/05/alternativa_1/activar_nota.s
+++ Soluciones/05/alternativa_1/activar_nota.s
@@ -8,17 +8,16 @@
     @; R2 = Volumen de la nota.
 activar_nota:
     push {r0-r4, lr}
-    
-    mov r3, r0, lsl #4
-    ldr r4, =0x04000400
-    orr r4, r3
+                                       
+    ldr r3, =0x04000400            @; Se carga la dirección base de los registros de control de cada canal                 
+    orr r3, r0, lsl #4             @; y se selecciona el registro de control correspondiente al canal que se está usando a través del propio canal (desplazado cuatro bits a la izquierda).
     orr r2, #0x88000000
-    str r2, [r4]
+    str r2, [r3]                   @; Se guarda el volumen de la nota en el registro de control a la vez que se activan los bits de incicio y de repetición activados
 
     ldr r0, =33513982
-    mov r0, r0, lsr #1
-    swi 9
-    rsb r0, r0, #0
-    strh r0, [r4, #4]
+    mov r0, r0, lsr #1             @; Se divide la frecuencia de entrada entre 2.
+    swi 9                          @; div_freq = freq_entrada / frec_salida
+    rsb r0, r0, #0                 @; Se pasa el valor del divisor de frecuencia a negativo debido a la propia naturaleza de este contador
+    strh r0, [r3, #4]              @; y se guarda en la posición correspondiente del registro de control.
     
     pop {r0-r4, pc}
