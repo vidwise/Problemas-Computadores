f391710c96f52ce3b0c494a3fa356e2cca0a3130
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-04 12:56:53 +0100

    Arey: Sustitución de tabuladores por espacios para cumplir con las directrices oficiales de estilo de programación.

--- Soluciones/05/alternativa_1/activar_nota.s
+++ Soluciones/05/alternativa_1/activar_nota.s
@@ -2,20 +2,20 @@
 @; Author: Arey Ferrero Ramos
 @;
 
-		@; Rutina para activar una nota.
-		@; R0 = Canal de sonido.
-		@; R1 = Frecuencia de la nota.
-		@; R2 = Volumen de la nota.
+    @; Rutina para activar una nota.
+    @; R0 = Canal de sonido.
+    @; R1 = Frecuencia de la nota.
+    @; R2 = Volumen de la nota.
 activar_nota:
-	push {r0-r4, lr}
-	mov r3, r0, lsl #4
-	ldr r4, =0x04000400
-	orr r4, r3
-	orr r2, #0x88000000
-	str r2, [r4]
-	ldr r0, =33513982
-	mov r0, r0, lsr #1
-	swi 9
-	rsb r0, r0, #0
-	strh r0, [r4, #4]
-	pop {r0-r4, pc}
+    push {r0-r4, lr}
+    mov r3, r0, lsl #4
+    ldr r4, =0x04000400
+    orr r4, r3
+    orr r2, #0x88000000
+    str r2, [r4]
+    ldr r0, =33513982
+    mov r0, r0, lsr #1
+    swi 9
+    rsb r0, r0, #0
+    strh r0, [r4, #4]
+    pop {r0-r4, pc}
--- Soluciones/05/alternativa_1/main.c
+++ Soluciones/05/alternativa_1/main.c
@@ -4,9 +4,9 @@
 
 typedef struct
 {
-	short freq;
-	short time;
-	short vol;
+    short freq;
+    short time;
+    short vol;
 } info_note;
 
 info_note musica[MAX_NOTAS];
@@ -17,18 +17,19 @@ unsigned char cambio_nota = 1;
 
 void main()
 {
-	inicializaciones();
-	pos_nota = 0;
-	tiempo_restante = musica[0].time;
-	activar_nota(0, musica[0].freq, musica[0].vol);
-	do
-	{
-		tareas_independientes();
-		swiWaitForVBlank();
-		if (cambio_nota)
-		{
-			printf("Nota actual: %i\n", pos_nota);
-			cambio_nota = 0;
-		}
-	} while(1);
+    inicializaciones();
+    
+    pos_nota = 0;
+    tiempo_restante = musica[0].time;
+    activar_nota(0, musica[0].freq, musica[0].vol);
+    do
+    {
+        tareas_independientes();
+        swiWaitForVBlank();
+        if (cambio_nota)
+        {
+            printf("Nota actual: %i\n", pos_nota);
+            cambio_nota = 0;
+        }
+    } while(1);
 }
