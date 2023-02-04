3e8b24ec16f9d27c26f0ca857cde68beb63f7112
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-04 20:08:25 +0100

    Arey: Addición de comentarios al main y a las RSIs del problema 35.

--- Soluciones/25/RSI_timer1.s
+++ Soluciones/25/RSI_timer1.s
@@ -6,33 +6,32 @@
 RSI_Timer1:
     push {r0-r1, lr}
     
-    ldr r0, =num_disp            @; Cargamos dir.mem num disp
-    ldrb r1, [r0]
+    ldr r0, =num_disp            
+    ldrb r1, [r0]                   @; Se carga el número de disparos.
     cmp r1, #0
     beq .Ldetener_disparos
-    
-    sub r1, #1
+                                    @; Si quedan disparos
+    sub r1, #1                      @; se disminuye el número de disparos.
     strb r1, [r0]
     
-    ldr r0, =REG_SHOOTER
+    ldr r0, =REG_SHUTER
     mov r1, #0x0010
-    strh r1, [r0]
+    strh r1, [r0]                   @; se activa el bit half press del registro REG_SHUTTER. 
     
-    ldr r0, =status
     mov r1, #1
-    strb r1, [r0]
-    
+    ldr r0, =status
+    strb r1, [r0]                   @; se pasa al estado 1.
     ldr r0, =actualizar_info
-    strb r1, [r0]
+    strb r1, [r0]                   @; se activa la variable booleana 'actualizar_info'.
     
     ldr r0, =350
-    bl activar_timer2
+    bl activar_timer2               @; se activa el timer 2 con un periodo de 350 ms.
     
     b .Lfin_detener_disparos
 
-.Ldetener_disparos:
+.Ldetener_disparos:                 @; Si no quedan disparos
     mov r0, #0x3
-    bl desactivar_timers
+    bl desactivar_timers            @; se paran los timers.
 
 .Lfin_detener_disparos:
     pop {r0-r1, pc}
--- Soluciones/25/main.c
+++ Soluciones/25/main.c
@@ -2,13 +2,13 @@
 // Author: Arey Ferrero Ramos
 //
 
-unsigned char num_disp = 0;         // Número de disparos pendientes
-unsigned char ind_p1 = 0;           // Índice del periodo p1
-unsigned char ind_p2 = 0;           // Índice del periodo p2
-unsigned char status = 0;           // Estado actual (0 --> parado,
-                                                   // 1 --> half press,
-                                                   // 2 --> half+full press)
-unsigned char actualizar_info = 0;  // Indica si hay que actualizar información en pantalla
+unsigned char num_disp = 0;           // Número de disparos pendientes
+unsigned char ind_p1 = 0;             // Índice del periodo p1
+unsigned char ind_p2 = 0;             // Índice del periodo p2
+unsigned char status = 0;             // Estado actual (0 --> parado,
+                                                     // 1 --> half press,
+                                                     // 2 --> half+full press)
+unsigned char actualizar_info = 0;    // Indica si hay que actualizar información en pantalla
 
 void main()
 {
@@ -21,24 +21,24 @@ void main()
         
         scanKeys();
         keys = keysDown();
-        if(keys & KEY_START)
-        {
-            if(num_disp == 0)
-            {
-                activar_timers01(1 << ind_p1);
+        if (keys & KEY_START)
+        {                                           // Si se ha pulsado la tecla KEY_START.
+            if (num_disp == 0)
+            {                                       // Si no se ha efectuado ningún disparo
+                activar_timers01(1 << ind_p1);      // se activan los timers para que se inicie los disparos.
             }
-            num_disp++;
+            num_disp++;                             // Se incrementa el número de disparos.
             actualizar_info = 1;
         }
         if ((keys & KEY_LEFT) && (ind_p1 > 0))
         {
-            activar_timers01(1 << --ind_p1);        // Primero decrementamos luego desplazamos bits
+            activar_timers01(1 << --ind_p1);        // Primero se decrementa el contador y después se lleva a cabo el desplazamiento de bits.
             actualizar_info = 1;
         }
         if ((keys & KEY_RIGHT) && (ind_p1 < 9))
         {
-            activar_timers01(1 << ++ind_p1);        // Primero incrementamos luego desplazamos bits
-            actualizar_info = 1;
+            activar_timers01(1 << ++ind_p1);        // Primero se incrementa el contador y después se lleva a cabo el desplazamiento de bits.
+            actualizar_info = 1;                    
         }
         if ((keys & KEY_DOWN) && (ind_p2 > 0))
         {
@@ -51,7 +51,7 @@ void main()
             actualizar_info = 1;
         }
         
-        swiWaitForVBlank();                         // Se pone fuera porque la evaluación de la condición apenas ralentiza el programa y no existirán problemas de colisiones
+        swiWaitForVBlank();                         // Se pone fuera porque la evaluación de la condición apenas ralentiza el programa y no existirán problemas de colisiones.
                                                     // Así, se disminuye el consumo de la CPU aunque no haya pulsación de teclas.
         if (actualizar_info == 1)
         {
