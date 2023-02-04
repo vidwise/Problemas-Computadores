8cdcf8badbd3f1141e172edb491c5d3f8aef64d6
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2023-02-04 13:28:37 +0100

    Arey: Addici ón de líneas en blanco a las rutinas en ensamblador del problema 8.

--- Soluciones/08/insertar_string.s
+++ Soluciones/08/insertar_string.s
@@ -7,17 +7,22 @@
     @; R1 = Comando para fijar la dirección base.
 insertar_string:
     push {r0-r3, lr}
+    
     ldr r2, =REG_DISPLAY
     bl sincro_display
     strh r1, [r2]
+    
     mov r1, #0
 .Lnueva_posicion:
+    
     ldrb r3, [r0, r1]
     orr r3, #0x200
     bl sincro_display
     strh r3, [r2]
+    
     add r1, #1
     cmp r1, #MAX_ELEMENTS
     blo .Lnueva_posicion
+    
     pop {r0-r3, pc}
 
--- Soluciones/08/insertar_strings.s
+++ Soluciones/08/insertar_strings.s
@@ -7,11 +7,14 @@
     @; R1 = str2.
 insertar_strings:
     push {r0-r2, lr}
+    
     mov r2, r1
     mov r1, #0x080
     bl insertar_string
+    
     mov r0, r2
     mov r1, #0x0C0
     bl insertar_string
+    
     pop {r0-r2, pc}
 
